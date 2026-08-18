8760b6d094800d2608501ca22969c2c21148762b
Mitansh Gole <golemitansh@gmail.com>
2026-08-18 18:30:15 +0530

    Add basic arithmetic operations in Q2.c

---
+++ Q2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    // Input two numbers
+    scanf("%d %d", &a, &b);
+
+    // Display results
+    printf("Sum=%d, Diff=%d, Product=%d, Quotient=%d", a + b, a - b, a * b, a / b);
+
+    return 0;
+}
