33dcb9b0888bdc1a5782596608ba71ae71491cdb
Mitansh Gole <golemitansh@gmail.com>
2026-08-24 22:00:40 +0530

    Implement factorial calculation in Day15 Q29.c

---
+++ Day15 Q29.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    long long factorial = 1;
+
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        factorial = factorial * i;
+    }
+
+    printf("%lld", factorial);
+
+    return 0;
+}
