05d696c0416f016ac6ed6d4d71b09d24d22caa64
Mitansh Gole <golemitansh@gmail.com>
2026-08-29 19:05:20 +0530

    Add binary inversion program
    Implement binary inversion in C.

---
+++ Day20 Q40.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char binary[100];
+
+    scanf("%s", binary);
+
+    for (int i = 0; binary[i] != '\0'; i++) {
+        if (binary[i] == '0')
+            printf("1");
+        else if (binary[i] == '1')
+            printf("0");
+    }
+
+    return 0;
+}
