c96b61ea30d5f3e3c3e9ee4ac8a9bc7fd7c92ee4
Mitansh Gole <golemitansh@gmail.com>
2026-09-12 19:43:50 +0530

    Implement array element removal by position
    This program reads an array, removes an element at a specified position, and prints the updated array.

---
+++ Day34 Q68.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100];
+    int n, pos, i;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &pos);
+
+    // Shift elements to the left
+    for (i = pos; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    n--;
+
+    // Print updated array
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
