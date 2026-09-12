e37e65c37d48979337ffc24006a01a0d829db83c
Mitansh Gole <golemitansh@gmail.com>
2026-09-12 19:42:34 +0530

    Implement array insertion at specified position
    This program reads an array, inserts a new element at a specified position, and prints the updated array.

---
+++ Day34 Q67.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100];
+    int n, pos, element, i;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d %d", &pos, &element);
+
+    // Shift elements to the right
+    for (i = n; i > pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    // Insert the new element
+    arr[pos] = element;
+    n++;
+
+    // Print the updated array
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
