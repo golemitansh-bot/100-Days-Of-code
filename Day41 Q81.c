192e8183deff835478c946d3e2030a988011bbab
Mitansh Gole <golemitansh@gmail.com>
2026-09-19 16:58:21 +0530

    Count non-newline characters in input string

---
+++ Day41 Q81.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i = 0, count = 0;
+
+    fgets(str, sizeof(str), stdin);
+
+    while(str[i] != '\0')
+    {
+        if(str[i] != '\n')
+        {
+            count++;
+        }
+        i++;
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
